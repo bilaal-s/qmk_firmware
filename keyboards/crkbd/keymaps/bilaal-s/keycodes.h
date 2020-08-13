2758c9da0c22c630ad0fc17e2ef54a9f2987a348
Bilaal Salad <bilaaldev@gmail.com>
2020-08-13 11:11:12 +0100

    feat: update keymap

--- keyboards/crkbd/keymaps/bilaal-s/keycodes.h
+++ keyboards/crkbd/keymaps/bilaal-s/keycodes.h
@@ -11,4 +11,4 @@ enum custom_keycodes { RGBRST };
 
 #define HCTL_TAB MT(MOD_LCTL, KC_TAB)
 #define HCTL_QT MT(MOD_LCTL, KC_QUOT)
-#define HRALT_SPC MT(MOD_RALT, KC_SPC)
+#define HRALT_ENT MT(MOD_RALT, KC_ENT)
