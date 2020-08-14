4ec7c11631a051ccdb5588a39332d13f74585302
Bilaal Salad <bilaaldev@gmail.com>
2020-08-14 13:29:57 +0100

    fix: rgbreset and toggle

--- keyboards/crkbd/keymaps/bilaal-s/keymap.c
+++ keyboards/crkbd/keymaps/bilaal-s/keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, KC_HOME,  KC_UP,   KC_END,  KC_PGUP,                      XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      _______, XXXXXXX, KC_LEFT,  KC_UP, KC_RIGHT,  KC_PGUP,                      XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_NUBS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+      _______, KC_NUBS, KC_HOME, KC_DOWN, KC_END, KC_PGDN,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           MO(3), _______, _______,     _______, _______, _______
                                       //`--------------------------'  `--------------------------'
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      _______, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_MOD,                      RGB_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, HCTL_QT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGBRST,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      _______, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGB_RMOD,                      RGBRST, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,   _______, _______, _______
                                       //`--------------------------'  `--------------------------'
@@ -66,11 +66,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
   switch (keycode) {
     case RGBRST:
-      #ifdef RGBLIGHT_ENABLE
+      #ifdef RGB_MATRIX_ENABLE
         if (record->event.pressed) {
           eeconfig_update_rgb_matrix_default();
           rgb_matrix_enable();
         }
+        return false;
       #endif
       break;
   }
